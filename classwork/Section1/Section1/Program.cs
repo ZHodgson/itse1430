5748156cfa9add1f2efbdb3350b1c7410e057ffb
zhodgson <zachary.hodgson455@my.tccd.edu>
2020-01-22 21:24:13 -0600

    Changes for class 1/22/20

--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * ITSE 1430
- * Zachary Hodgson
+ * Your Name
  */
 using System;
 
@@ -10,18 +10,88 @@ namespace Section1
     {
         static void Main ( string[] args )
         {
+            //PlayingWithVariables();                       
+            AddMovie();
+        }
+
+        static void AddMovie ()
+        {
+            string title = ReadString("Enter a title: ", true);
+
+            int releaseYear = ReadInt32("Enter the release year (>= 0): ", 0, 2100);
+            int runLength = ReadInt32("Enter the run length (>= 0): ", 0, 86400);
+
+            string description = ReadString("Enter a description: ", false);
+            bool isClassic = ReadBoolean("Is this a classic movie?");
+        }
+
+        private static bool ReadBoolean ( string message )
+        {
+
+            Console.Write(message + " (Y/N)");
+            string value = Console.ReadLine();
+            //TODO: do this correctly
+            char firstChar = value[0];
+            return firstChar == 'Y';
+
+        }
+
+        private static string ReadString (string message, bool required)
+        {
+            Console.Write(message);
+            string value = Console.ReadLine();
+
+            //TODO: Validate
+            return value;
+        }
+
+        private static int ReadInt32 ( string message, int minValue, int maxValue )
+        {
+            Console.Write(message);
+
+            string temp = Console.ReadLine();
+            //int value = Int32.Parse(temp);
+            int value;
+            if (Int32.TryParse(temp, out value))
+                return value;
+
+            //TODO: clean it up
+            return -1;
+        }
+
+        private static void PlayingWithVariables ()
+        {
             Console.WriteLine("Hello World!");
 
             int hours;
             double payRate;
-            string name;
-            bool pass;
-            //Logical block 1
-            int hours2 = 10;
-            int x, y, z;
-            int a = 19, b = 20, c = 30;
 
-            //Logical blocks 2
+            {
+                string name;
+                bool pass;
+            }
+
+            {
+                int name = 10;
+            }
+
+            //Not cased properly
+            //string FirstName;
+
+            //hours = 10;
+            //int newHours = hours;
+
+            //Logical block 1     
+            int hours2 = 10;
+            //int hours3;
+            //hours3 = 3;
+
+            int x, y, z;
+            int a = 10, b = 20, c = 30;
+
+            //Display a message
+            Console.WriteLine("Enter a value");
+            string input = Console.ReadLine();
         }
     }
 }
